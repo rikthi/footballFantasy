0a16aaa374f5fcf922436b317be135597c1ae0bd
Rikthi Pranadhik Mondal <147805511+rikthi@users.noreply.github.com>
2024-09-10 17:26:11 -0600

    Starting project

---
+++ src/main/java/com/ff/footballfantasy/FootballFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.ff.footballfantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFantasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballFantasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ff/footballfantasy/FootballFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.footballfantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFantasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
